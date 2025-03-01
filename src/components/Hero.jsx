b7be6430d66d91ac287f3cda22125ef229fdeaf7
Dali Duraku <mesherbetmolla@gmail.com>
2025-03-01 15:57:18 -0500

    feat: hero and form pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,15 @@ export default function Hero() {
             </div>
             <div className="card info-card">
                 <div>
+                    <i className="fa-solid fa-circle-info"></i>
                     <h3>Did you know...</h3>
                 </div>
+                <h5>That caffeine&apos;s half-life is about 5 hours?</h5>
+                <p>
+                    This means that after 5 hours, half the caffeine you consumed is still in your system, 
+                    keeping you alert longer! So if you drink a cup of coffee with 200mg of caffeine, 5 hours, 
+                    later you&apos;ll still have about 100mg of caffeine in your system.
+                </p>
             </div>
         </>
     )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,22 @@
 export default function Layout(props) {
     const { children } = props
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">ESPRESSOFIEND</h1>
+                <p>For Coffee Insatiates</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i className="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
 
     const footer = (
-        <footer></footer>
+        <footer>
+            <p><span className="text-gradient">Espressofiend</span> was made by <a target="_blank" href="https://github.com/daliduraku">Dali Duraku</a></p>
+        </footer>
     )
     return(
         <>
