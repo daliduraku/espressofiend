8f6b380a56bad13ea64bdedcd910d101076b4b9d
Dali Duraku <mesherbetmolla@gmail.com>
2025-03-01 14:51:12 -0500

    Update to title and icon of the web app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,18 @@
 export default function Hero() {
     return (
         <>
-            
+            <h1>Coffee Tracking for Coffee <abbr title="An enthusiast or devotee">Fiends!</abbr></h1>
+            <div className="benefits-list">
+                <h3 className="font-bolder">Try <span className="text-gradient">Espressofiend</span> and start ...</h3>
+                <p>✅ Tracking every coffee</p>
+                <p>✅ Measuring your blood caffeine levels</p>
+                <p>✅ Costing and quantifying your addiction</p>
+            </div>
+            <div className="card info-card">
+                <div>
+                    <h3>Did you know...</h3>
+                </div>
+            </div>
         </>
     )
 }
