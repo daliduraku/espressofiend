e0f3b8de23f71da6a83ac99f15e7ef765aea3031
Dali Duraku <mesherbetmolla@gmail.com>
2025-03-01 20:09:19 -0500

    feat: form complete and stats information

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,8 +1,27 @@
+import { coffeeConsumptionHistory } from "../utils";
 
 export default function History() {
     return (
         <>
-            
+            <div className="section-header">
+                <i className="fa-solid fa-timeline" />
+                <h2>History</h2>
+            </div>
+            <p><i>Hover for more information</i></p>
+            <div className="coffee-history">
+                {Object.keys(coffeeConsumptionHistory).sort((a,b) => b - a).map((utcTime, coffeeIndex) => {
+                    const coffee = 
+
+
+
+                    
+                    return(
+                        <div key={coffeeIndex}>
+                            <i className="fa-solid fa-mug-hot" />
+                        </div>
+                    )
+                })}
+            </div>
         </>
     )
 }
