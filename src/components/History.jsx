b9087312b19043715b3c05c5a18cb88f21c214e4
Dali Duraku <mesherbetmolla@gmail.com>
2025-03-01 20:39:21 -0500

    feat: history consumption of coffees

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,4 +1,4 @@
-import { coffeeConsumptionHistory } from "../utils";
+import { calculateCurrentCaffeineLevel, coffeeConsumptionHistory, getCaffeineAmount, timeSinceConsumption } from "../utils";
 
 export default function History() {
     return (
@@ -9,18 +9,26 @@ export default function History() {
             </div>
             <p><i>Hover for more information</i></p>
             <div className="coffee-history">
-                {Object.keys(coffeeConsumptionHistory).sort((a,b) => b - a).map((utcTime, coffeeIndex) => {
-                    const coffee = 
-
-
-
+            {Object.keys(coffeeConsumptionHistory).sort((a,b) => b - a).map((utcTime, coffeeIndex) => {
                     
+                    const coffee =  coffeeConsumptionHistory[utcTime]
+                    const timeSinceConsume = timeSinceConsumption(utcTime)
+                    const originalAmount = getCaffeineAmount(coffee.name)
+                    const remainingAmount = calculateCurrentCaffeineLevel({
+                        [utcTime]: coffee
+                    })
+
+                    const summary = `${coffee.name} | ${timeSinceConsume} | $${coffee.cost} | ${remainingAmount}mg / ${originalAmount}mg`
+
                     return(
-                        <div key={coffeeIndex}>
+                        <div 
+                            key={coffeeIndex}
+                            title={summary}
+                        >
                             <i className="fa-solid fa-mug-hot" />
                         </div>
                     )
-                })}
+            })}
             </div>
         </>
     )
