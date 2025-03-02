828a126d326e8626060b87ff0c03cd20f90de587
Dali Duraku <mesherbetmolla@gmail.com>
2025-03-02 15:35:05 -0500

    feat: final espressofiend app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import Layout from './components/Layout';
-import Hero from './components/Hero';
-import CoffeeForm from './components/CoffeeForm'
-import Stats from './components/Stats'
-import History from './components/History'
+import CoffeeForm from "./components/CoffeeForm"
+import Hero from "./components/Hero"
+import History from "./components/History"
+import Layout from "./components/Layout"
+import Stats from "./components/Stats"
+import { useAuth } from "./context/AuthContext"
+import { coffeeConsumptionHistory } from "./utils"
 
 function App() {
-  
-  const isAuthenticated = true;
+  const { globalUser, isLoading, globalData } = useAuth()
+  const isAuthenticated = globalUser
+  const isData = globalData && !!Object.keys(globalData || {}).length
 
   const authenticatedContent = (
     <>
@@ -19,8 +21,11 @@ function App() {
   return (
     <Layout>
       <Hero />
-      <CoffeeForm isAuthenticated={isAuthenticated}/>
-      {isAuthenticated && authenticatedContent}
+      <CoffeeForm isAuthenticated={isAuthenticated} />
+      {(isAuthenticated && isLoading) && (
+        <p>Loading data...</p>
+      )}
+      {(isAuthenticated && isData) && (authenticatedContent)}
     </Layout>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,32 @@
 import Modal from "./Modal"
 import Authentication from "./Authentication"
 import { useState } from "react"
+import { useAuth } from "../context/AuthContext"
 
 export default function Layout(props) {
     const { children } = props
     const [showModal, setShowModal] = useState(false)
+    const { globalUser, logout } = useAuth()
+
     const header = (
         <header>
             <div>
                 <h1 className="text-gradient">ESPRESSOFIEND</h1>
                 <p>For Coffee Insatiates</p>
             </div>
-            <button
-                onClick={() => {setShowModal(true)}}
-            >
-                <p>Sign up free</p>
-                <i className="fa-solid fa-mug-hot"></i>
-            </button>
+            {globalUser ? 
+               (<button
+                onClick={logout}
+                >
+                    <p>Logout</p>
+                </button>) :
+               (<button
+                    onClick={() => {setShowModal(true)}}
+                >
+                    <p>Sign up free</p>
+                    <i className="fa-solid fa-mug-hot"></i>
+                </button>)
+            }
         </header>
     )
 
@@ -25,13 +35,20 @@ export default function Layout(props) {
             <p><span className="text-gradient">Espressofiend</span> was made by <a target="_blank" href="https://github.com/daliduraku">Dali Duraku</a></p>
         </footer>
     )
+
+    function handleCloseModal() {
+        setShowModal(false)
+    }
+
     return(
         <>
             {showModal && (
                 <Modal
-                    handleCloseModal={() => {setShowModal(false)}}
+                    handleCloseModal={handleCloseModal}
                 >
-                    <Authentication />
+                    <Authentication 
+                        handleCloseModal={handleCloseModal}
+                    />
                 </Modal>
             )}
             {header}
